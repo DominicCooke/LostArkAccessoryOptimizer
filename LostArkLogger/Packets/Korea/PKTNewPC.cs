b32ace4f4b01af7cf509bf28e117d662e09e1a18
FRK <cookedomi@gmail.com>
2022-08-11 10:26:38 +0100

    Trying to update for new patch working

--- LostArkLogger/Packets/Korea/PKTNewPC.cs
+++ LostArkLogger/Packets/Korea/PKTNewPC.cs
@@ -6,7 +6,6 @@ namespace LostArkLogger
     {
         public void KoreaDecode(BitReader reader)
         {
-            pCStruct = reader.Read<PCStruct>();
             b_0 = reader.ReadByte();
             b_1 = reader.ReadByte();
             if (b_1 == 1)
@@ -15,12 +14,13 @@ namespace LostArkLogger
             if (b_2 == 1)
                 bytearray_0 = reader.ReadBytes(20);
             b_3 = reader.ReadByte();
-            if (b_3 == 1)
-                u32_0 = reader.ReadUInt32();
             b_4 = reader.ReadByte();
             if (b_4 == 1)
                 subPKTNewPC33 = reader.Read<subPKTNewPC33>();
             b_5 = reader.ReadByte();
+            if (b_5 == 1)
+                u32_0 = reader.ReadUInt32();
+            pCStruct = reader.Read<PCStruct>();
         }
     }
 }
--- LostArkLogger/Packets/Korea/SkillDamageEvent.cs
+++ LostArkLogger/Packets/Korea/SkillDamageEvent.cs
@@ -6,14 +6,14 @@ namespace LostArkLogger
     {
         public void KoreaDecode(BitReader reader)
         {
-            Modifier = reader.ReadByte();
             b_0 = reader.ReadByte();
-            u16_0 = reader.ReadUInt16();
-            b_1 = reader.ReadByte();
-            if (b_1 == 1)
-                b_2 = reader.ReadByte();
-            CurHp = reader.ReadPackedInt();
+            if (b_0 == 1)
+                b_1 = reader.ReadByte();
             Damage = reader.ReadPackedInt();
+            b_2 = reader.ReadByte();
+            u16_0 = reader.ReadUInt16();
+            CurHp = reader.ReadPackedInt();
+            Modifier = reader.ReadByte();
             TargetId = reader.ReadUInt64();
             MaxHp = reader.ReadPackedInt();
         }
